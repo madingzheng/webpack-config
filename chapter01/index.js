d5aed96ea09518267d2023b4f0032b7712c4cfd6
马定争V <V_ma_dingzheng@dahuatech.com>
2020-07-07 16:57:51 +0800

    chapter01

---
+++ chapter01/index.js
@@ -0,0 +1,8 @@
+// const path = require('path');
+
+// console.log(__dirname);
+// console.log(__dirname + '/src');
+// console.log(path.resolve(__dirname));
+// console.log(path.resolve(__dirname, './src'));
+
+console.log(process);
---
+++ chapter01/src/es/index.js
@@ -0,0 +1,19 @@
+// import hello from './hello';
+
+// import world from './world';
+
+// const city = address?.city
+
+// console.log(111);
+// console.log(city);
+// console.log(...[1,2,3]);
+// console.log(`${hello}${world}`);
+
+
+// import { name } from '@utils/index';
+// console.log(name);
+
+import html from './index.html';
+import './index.less';
+
+console.log(html);
